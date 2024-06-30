a164a885c8e685c435b093dba4f04531f9fe4190
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-06-30 01:45:37 -0300

    ok

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
-import (
-	tester "github.com/rzjprogramador/Pgm_Universal/Pgm/literals/tester"
-)
+import "github.com/rzjprogramador/Pgm_Universal/Pgm/linguagem/literals/tester"
 
 func main() {
 	tester.AllLinguagemRepositoryMemory_Tester()
 	tester.Typescript_Tester()
 	tester.Golang_Tester()
-	tester.UniversalPGM_Tester()
+	// tester.UniversalPGM_Tester()
 }
