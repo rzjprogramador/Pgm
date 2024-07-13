e73faa323698ffaec8318b65c63b68aed4ea5407
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-07-13 20:25:49 -0300

    ok

--- Exemplos/EntidadeFuncionalidade.ts
+++ Exemplos/EntidadeFuncionalidade.ts
@@ -1,13 +1,14 @@
+// Contratos : [ EntidadeFuncionalidade, EntidadeFuncionalidadeFNReturn]
 interface EntidadeFuncionalidade {
   c1: number
   c2: number
 }
 
-type EntidadeFuncionalidadeFNReturn = number
+type Action1Return = number
 type EntidadeFuncionalidadeInput = EntidadeFuncionalidade | EntidadeFuncionalidadeStruct
 type Test_EntidadeFuncionalidadeType = Error | void
 
-// Contrato_Estruturador
+// Contrato_Estruturador :
 class EntidadeFuncionalidadeStruct {
   public readonly c1: number = 0
   public readonly c2: number = 0
@@ -17,41 +18,44 @@ class EntidadeFuncionalidadeStruct {
     this.c2 = x.c2
   }
 
-  // acao_das_intancias_filhas
+  // acao : das_intancias_filhas
+  // gerar novas instancias
   static New(x: EntidadeFuncionalidade) {
     return new EntidadeFuncionalidadeStruct(x)
   }
 
-  //
-  static ActionSoma(x: EntidadeFuncionalidadeInput): number {
+  // acao: das instancias : somar
+  static Action1(x: EntidadeFuncionalidadeInput): Action1Return {
     const formula = x.c1 + x.c2
     return formula
   }
 }
 
 
-// input de dados sejam eles externos, hardcorde, doCodigo
-const EntidadeFuncionalidadeInput_OK = EntidadeFuncionalidadeStruct.ActionSoma({ c1: 10, c2: 10 })
-const EntidadeFuncionalidadeInput_FAIL = EntidadeFuncionalidadeStruct.ActionSoma({ c1: 20, c2: 20 })
+// input: de dados sejam eles externos, hardcorde, doCodigo
+const EntidadeFuncionalidadeInput_OK = EntidadeFuncionalidadeStruct.Action1({ c1: 10, c2: 10 })
+const EntidadeFuncionalidadeInput_FAIL = EntidadeFuncionalidadeStruct.Action1({ c1: 20, c2: 20 })
 const EntidadeFuncionalidadeInput_HARD = { c1: 10, c2: 10 }
 
-
-function Test_EntidadeFuncionalidadeFN(op: EntidadeFuncionalidadeFNReturn): Test_EntidadeFuncionalidadeType {
+// test :
+function Test_EntidadeFuncionalidadeFN(op: Action1Return): Test_EntidadeFuncionalidadeType {
   if (op != 20) {
     throw new Error(`OPS..O TEST_FALHOU : O resultado = ${op}`)
   }
   console.log(`Ok..O TEST_PASSOU : O resultado = ${op}`)
 }
 
-const resultTest_EntidadeFuncionalidade_OK = () => Test_EntidadeFuncionalidadeFN(EntidadeFuncionalidadeInput_OK)
-const resultTest_EntidadeFuncionalidade_FAIL = () => Test_EntidadeFuncionalidadeFN(EntidadeFuncionalidadeInput_FAIL)
+// testerResult : para gerar resultados para serem usados no test
+const entidadeFuncionalidade_TesterResult_OK = () => Test_EntidadeFuncionalidadeFN(EntidadeFuncionalidadeInput_OK)
+const entidadeFuncionalidade_TesterResult_FAIL = () => Test_EntidadeFuncionalidadeFN(EntidadeFuncionalidadeInput_FAIL)
 
-function RunTests() {
-  resultTest_EntidadeFuncionalidade_OK()
+// runAllTests : rodar todos tests existentes
+function RunAllTests() {
+  entidadeFuncionalidade_TesterResult_OK()
   // resultTest_EntidadeFuncionalidade_FAIL()
 }
 
-RunTests()
+RunAllTests()
 
 /*
 ALGORITMOS PARA ENTIDADE OU FUNCIONALIDADES
